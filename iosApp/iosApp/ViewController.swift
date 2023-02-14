d7cd5fd63360592fc0f79dd47e17e756f6b314be
jerryqin <jerryqqh@gmail.com>
2023-02-14 11:53:08 +0800

    add project

---
+++ iosApp/iosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosApp
+//
+//  Created by jerryqin on 2023/2/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
