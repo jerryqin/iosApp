a0a99f3dd89bdb0e84d10bdc5c58baf6d3b5d6a5
jerryqin <jerryqqh@gmail.com>
2023-02-14 12:03:05 +0800

    print 2 dev

--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print(2)
+        print(2,"dev")
     }
 
 }
